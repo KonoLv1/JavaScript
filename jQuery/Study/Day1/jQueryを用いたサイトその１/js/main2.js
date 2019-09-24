cd92ee2320896fde35170e935968c8c41c17bc32
Konolv1 <konolevel1@gmail.com>
2019-09-24 13:06:07 +0900

    jQueryを用いたサイト制作

--- jQuery/Study/Day1/jQueryを用いたサイトその１/js/main2.js
+++ jQuery/Study/Day1/jQueryを用いたサイトその１/js/main2.js
@@ -1,13 +1,104 @@
+/*
+// ハローと表示する
+alert("現在制作中です。");
+*/
+/*
+$(function(){
+    $(".hoge1").css("color","blue")
+});
+*/
+
+/*
+$(function(){
+    $('.slideshow').each(function(){
+        // 変数を準備
+        var $container = $(this),
+            $slideGroup = $container.find('.slideshow-slides'),
+            $slides = $slideGroup.find('.slide'),
+            $nav = $container.find('.slideshow-nav'),
+            $indicator = $container.find('.slideshow-indicator'),
+
+            slideCount = $slides.length,
+            indicatorHTML = '',
+            currentIndex = 0,
+            duration = 500,
+            easing = 'easeInOutExpo',
+            interval = 7500,
+            timer;
+
+            // HTML要素の配置、生成、挿入
+
+            $slides.each(function(i){
+                $(this).css({left: 100 * i + '%'});
+                indicatorHTML += '<a href="#">' + (i + 1) + '</a>';
+            });
+
+            $indicator.html(indicatorHTML);
+
+            function goToSlide(index){
+                $slideGroup.animate({left: - 100 * index + '%'},duration,easing);
+                currentIndex = index;
+                updateNav();
+            }
+            function updateNav(){
+                var $navPrev = $nav.find('.prev'),
+                    $navNext = $nav.find('.next');
+
+                    if (currentIndex === 0){
+                        $navPrev.addClass('disabled');
+                    }else{
+                        $navPrev.removeClass('disabled');
+                    }
+
+                    if (currentIndex === slideCount - 1){
+                        $navNext.addClass('disabled');
+                    }else{
+                        $navNext.removeClass('disabled');
+                    }
+                    $indicator.find('a').removeClass('active')
+                    .eq(currentIndex).addClass('active');
+            }
+
+            function startTimer(){
+                timer = setInterval(function(){
+                    var nextIndex = (currentIndex + 1) % slideCount;
+                    goToSlide(nextIndex);
+                }, interval);
+            }
+
+            function stopTimer(){
+                clearInterval(timer);
+            }
+
+            $nav.on('click', 'a', function (event){
+                event.preventDefault();
+                if($(this).hasClass('prev')){
+                    goToSlide(currentIndex - 1);
+                }else{
+                    goToSlide(currentIndex + 1);
+                }
+            });
+
+            $indicator.on('clicl','a',function (event){
+                event.preventDefault();
+                if (!$(this).hasClass('active')){
+                    goToSlide($(this).index());
+                }
+            });
+            $container.on({
+                mouseenter: stopTimer,
+                mouseleave: startTimer
+            });
+
+            goToSlide(currentIndex);
+            startTimer();
+    });
+});
+
+*/
+
 $(function () {
 
-
-
-    setTimeout(function(){
-		$('.start p').fadeIn(1600);
-	},500); //0.5秒後にロゴをフェードイン!
-	setTimeout(function(){
-		$('.start').fadeOut(500);
-	},2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
     /*
      * Slideshow
      */
